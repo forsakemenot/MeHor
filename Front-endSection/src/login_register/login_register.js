376f1e1af88f43307cc9d5e60f2e68d36cbe9ca2
61070071 <61070071@kmitl.ac.th>
2021-04-04 01:14:48 +0700

    update login

--- Front-endSection/src/login_register/login_register.js
+++ Front-endSection/src/login_register/login_register.js
@@ -1,11 +1,17 @@
 import logo from './../img/MeHor.svg';
 import email from './../img/Icon-zocial-email.svg';
 import lock from './../img/Icon-material-lock.svg';
-import React from "react";
+import man from './../img/Icon-awesome-user-alt.svg';
+import React, { useState } from 'react';
 import './login_register.css';
 import './../App.css';
 import { Link } from "react-router-dom";
-function login_register() {
+
+function Login_register() {
+    // state = {
+    //     activeTab: 'tab1'
+    // }
+    const [activeTab, setActiveTab] = useState("tab2");
     return (
 
         <div>
@@ -16,33 +22,83 @@ function login_register() {
             </div>
             <div className="box_login">
                 <div className="d-flex">
-                    <div className="tab_child text_tab active">เข้าสู่ระบบ</div>
-                    <div className="tab_child text_tab">สมัครสมาชิก</div>
+                    <div onClick={() => setActiveTab("tab1")} className={`tab_child text_tab ${activeTab === "tab1" && "active"}`}>เข้าสู่ระบบ</div>
+                    <div onClick={() => setActiveTab("tab2")} className={`tab_child text_tab ${activeTab === "tab2" && "active"}`}>สมัครสมาชิก</div>
                 </div>
-                <div className="d-flex text_from">
-                    <p>E-mail</p>
+                {
+                    activeTab === "tab1" &&
                     <div>
-                        <input type="text"></input>
-                        <img id="img_email" alt="" src={email} />
+                        <div className="d-flex text_from">
+                            <p>E-mail</p>
+                            <div>
+                                <input type="text"></input>
+                                <img id="img_email" alt="" src={email} />
+                            </div>
+                        </div>
+                        <div className="d-flex text_from">
+                            <p>Password</p>
+                            <div>
+                                <input type="text"></input>
+                                <img id="img_lock" alt="" src={lock} />
+                            </div>
+                        </div>
+                        <div className="d-flex">
+                            <button className="btn_login">เข้าสู่ระบบ</button>
+                        </div>
+                        <div className="d-flex">
+                            <p className="text_forget"><Link to="/">ลืมรหัสผ่าน</Link> ใช่หรือไม่</p>
+                        </div>
                     </div>
-                </div>
-                <div className="d-flex text_from">
-                    <p>Password</p>
+                }
+                {
+                    activeTab === "tab2" &&
                     <div>
-                        <input type="text"></input>
-                        <img id="img_lock" alt="" src={lock} />
+                        <div className="d-flex text_from direction-row">
+                            <div className="flex-50">
+                                <p>ชื่อ</p>
+                                <div>
+                                    <input type="text"></input>
+                                    <img id="img_man1" alt="" src={man} />
+                                </div>
+                            </div>
+                            <div className="flex-50">
+                                <p>นามสกุล</p>
+                                <div>
+                                    <input type="text"></input>
+                                    <img id="img_man2" alt="" src={man} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="d-flex text_from">
+                            <p>E-mail</p>
+                            <div>
+                                <input type="text"></input>
+                                <img id="img_email2" alt="" src={email} />
+                            </div>
+                        </div>
+                        <div className="d-flex text_from">
+                            <p>Password</p>
+                            <div>
+                                <input type="text"></input>
+                                <img id="img_lock" alt="" src={lock} />
+                            </div>
+                        </div>
+                        <div className="d-flex text_from">
+                            <p>Confirm Password</p>
+                            <div>
+                                <input type="text"></input>
+                                <img id="img_lock" alt="" src={lock} />
+                            </div>
+                        </div>
+                        <div className="d-flex">
+                            <button className="btn_login m-b-2">สมัครสมาชิก</button>
+                        </div>
                     </div>
-                </div>
-                <div className="d-flex">
-                <button className="btn_login">เข้าสู่ระบบ</button>
-                </div>
-                <div className="d-flex">
-                <p className="text_forget"><Link to="/">ลืมรหัสผ่าน</Link> ใช่หรือไม่</p>
-                </div>
+                }
             </div>
         </div>
 
     );
 }
 
-export default login_register;
+export default Login_register;
