a8f72c0aa3f8d55d5a04d6a991cad461fdc83314
61070071 <61070071@kmitl.ac.th>
2021-05-04 02:16:32 +0700

    update dom me

---
+++ Front-endSection/src/components/ListDormMe/ListDormMe.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import del from '../../img/icon-red-del.svg';
+import dorm from "../../img/img_test.svg"
+import edit from "../../img/icon-edit.svg"
+function ListDormMe({ data }) {
+
+
+    return (
+        <div className="test_check d-flex align-items-center w-95 mt-1-v mb-1-v">
+            <div className="d-flex w-25 h-100">
+                <img alt="" src={dorm} className="w-100 h-100" />
+            </div>
+            <div className="d-flex w-60 justify-content-start h-80">
+                <div className="d-flex flex-column w-15 mx-auto align-items-end justify-content-around">
+                    <span className="fs-1-v color-main">ชื่อหอพัก :</span>
+                    <span className="fs-1-v color-main">สถานะ :</span>
+                    <span className="fs-1-v color-main">การปรับปรุง :</span>
+                </div>
+                <div className="d-flex flex-column w-80 h-100 mx-auto align-items-start justify-content-around">
+                    <span className="fs-1-v color-main font-weight-bold">{data.name}</span>
+                    {data.status === true ?
+                        <div className="bg-success rounded fs-0-8-v btn-check-status">
+                            <span className="color-white">ตรวจสอบแล้ว</span>
+                        </div> :
+                        <div className="bg-warning rounded fs-0-8-v btn-check-status">
+                            <span className="color-white">รอการตรวจสอบ</span>
+                        </div>
+                    }
+                    <span className="fs-1-v">(ปรับปรุงล่าสุด {data.update})</span>
+                </div>
+            </div>
+            <div className="d-flex w-15 align-items-start justify-content-end h-100">
+                <img alt="" src={edit} className="pointer image-del-dorm-me" />
+                <img alt="" src={del} className="pointer image-del-dorm-me" />
+
+            </div>
+        </div>
+
+    );
+}
+
+
+export default ListDormMe;
--- Front-endSection/src/pages/dom_me/dom_me.js
+++ Front-endSection/src/pages/dom_me/dom_me.js
@@ -3,54 +3,42 @@ import './dom_me.css';
 import '../../App.css';
 
 import { Link } from "react-router-dom";
-
 import city from '../../img/city.svg';
+import ListDormMe from "../../components/ListDormMe/ListDormMe"
 // import warningBW from '../../img/warning-bw.svg';
 
 function DomMe() {
+    const status = [{
+        name: "หอพักสันติ",
+        status: false,
+        update: "04/02/2564"
+    },
+    {
+        name: "หอพักสันติ",
+        status: false,
+        update: "04/02/2564"
+    }]
     return (
         <div id="form_bg" className="d-flex justify-center">
             <div className="dom_me d-flex">
                 <div className="header d-flex">
                     <div className="header_awar d-flex">
-                        <img alt="" className="img_doc" src={city}/>
-                        <p className="text_header">ประกาศหอพักของฉัน</p>
+                        <img alt="" className="img_doc" src={city} />
+                        <span className="text_header">ประกาศหอพักของฉัน</span>
                     </div>
 
                     <div className="add_room">
-                        <Link to="/AddDom"><button id="btn_continue" style={{width: '13vw'}}>+ ลงประกาศหอพัก</button></Link>
+                        <Link to="/AddDom"><button id="btn_continue" style={{ width: '13vw' }}>+ ลงประกาศหอพัก</button></Link>
                     </div>
                 </div>
 
                 <div className="apartment_success d-flex">
-                    {/* <img src={warningBW} style={{width: '2vw', margin: '0 1% 0 0'}}></img>
-                    <p>คุณยังไม่มีประกาศของหอพัก</p> */}
-
-                    <div className="test_check">
-                        <div className="img_test"></div>
-                        <div className="detail_check">
-                            <div className="static_box d-flex align-baseline">
-                                <p className="static_text">ชื่อหอพัก : </p>
-                                <p className="detail_text">เอแอนด์เจ แมนชั่น</p>
-                            </div>
-
-                            <div className="static_box d-flex align-baseline">
-                                <p className="static_text">สถานะ : </p>
-                                <p className="detail_status">รอการตรวจสอบ</p>
-                            </div>
-
-                            <div className="static_box d-flex align-baseline">
-                                <p className="static_text">การปรับปรุง : </p>
-                                <p className="detail_improve">(ปรับปรุงล่าสุด 04/02/2564)</p>
-                            </div>
-                        </div>
-                        <div>
-                            <select>
-                                <option>แก้ไข</option>
-                                <option>ลบ</option>
-                            </select>
-                        </div>
-                    </div>
+                    {
+                        status.map(function (element,index) {
+                            return <ListDormMe data={element} />
+                        })
+                    }
+                    {/* <ListDormMe data={status[0]} /> */}
                 </div>
             </div>
         </div>
