e83a50e5bb5f5cf9a5bda18345ee6f7cb23fabc9
61070071 <61070071@kmitl.ac.th>
2021-05-04 00:37:23 +0700

    edit navbar

--- Front-endSection/src/components/NavBar/NavBar.js
+++ Front-endSection/src/components/NavBar/NavBar.js
@@ -57,8 +57,8 @@ function NavBar() {
               <img alt="" src={arrow} className="image_search_arrow" onClick={toggleDropdownProfile} />
               {dropDownProfile === true &&
                 <div className="d-flex bg-white position-absolute align-items-center flex-column z-index-2 box-drop-down-profile rounded w-100">
-                  <span className="text-dark py-0-5-v fs-0-8-v">จัดการบัญชี</span>
-                  <Link to="AdminApprove" onClick={() => window.location.href = '/AdminDorm'}><span className="text-dark py-0-5-v fs-0-8-v">ประกาศหอพักของฉัน</span></Link>
+                  <Link to="/Profile" className="py-0-5-v" onClick={toggleDropdownProfile}><span className="text-dark fs-0-8-v">จัดการบัญชี</span></Link>
+                  <Link to="/AdminApprove" onClick={() => window.location.href = '/AdminDorm'}><span className="text-dark py-0-5-v fs-0-8-v">ประกาศหอพักของฉัน</span></Link>
                   <form onSubmit={handleLogout} className="py-0-5-v w-90 mx-auto">
                     <button className="w-100 color-main bg-white rounded fs-0-8-v">ออกจากระบบ</button>
                   </form>
--- Front-endSection/src/pages/admin/admin_approve/admin_approve.js
+++ Front-endSection/src/pages/admin/admin_approve/admin_approve.js
@@ -20,7 +20,7 @@ function AdminApprove() {
                     <span>รายการหอพักรออนุมัติ</span>
                     <span>Welcome! - ADMIN POWER</span>
                 </div>
-                <div className="form_panel flex-column px-1-v mt-2-v mb-2-v">
+                <div className="w-90 form_panel flex-column px-1-v mt-2-v mb-2-v">
                     <div className="d-flex align-items-center w-100 py-0-5-v">
                         <div className="w-50 d-flex align-items-center justify-content-start position-relative">
                             <input className="search_dorm_admin fs-1-v px-0-5-v" type="text" placeholder="รายการหอพักทั้งหมดในระบบ"></input>
